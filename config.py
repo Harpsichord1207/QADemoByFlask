c8a863ff0cbe1844db59ee1935bc0eafe1de46a0
Harpsichord <tliu1217@163.com>
2018-01-24 16:14:11 +0800

    change db password

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ HOST = "127.0.0.1"
 PORT = "3306"
 DB = "harp"
 USER = "root"
-PASS = "Harp@6585441"
+PASS = "123456"
 CHARSET = "utf8"
 DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}?charset={}".format \
 (USER, PASS, HOST, PORT, DB, CHARSET)
